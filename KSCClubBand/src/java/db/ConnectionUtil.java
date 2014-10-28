f1e3b24f7886f7c0309e615e272990cdbba14f0d
Brotherhood2014 <ducndgc00467@fpt.edu.vn>
2014-10-28 16:02:30 +0700

    Revert 8c6323e..9e80542
    This rolls back to commit 8c6323e1b37725ed89939b11669ebf5ca86e48df.

--- KSCClubBand/src/java/db/ConnectionUtil.java
+++ KSCClubBand/src/java/db/ConnectionUtil.java
@@ -8,7 +8,7 @@ public class ConnectionUtil {
     public static Connection getConnection() {
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            return DriverManager.getConnection("jdbc:sqlserver://localhost:1433;DatabaseName=KSCManagementEvent", "sa", "sa123");
+            return DriverManager.getConnection("jdbc:sqlserver://MIKE-PC:1433;DatabaseName=KSCManagementEvent", "sa", "root2014");
         } catch (ClassNotFoundException ex) {
             ex.printStackTrace();
         } catch (SQLException se) {
